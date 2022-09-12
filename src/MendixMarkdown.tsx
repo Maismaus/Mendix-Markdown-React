53d2a36a378cd8806fddeae6ef5e47b827348e0c
Maismaus <mail@mariusvanderknaap.nl>
2022-09-12 15:30:05 +0200

    Finalize eventlistener hooks code

--- src/MendixMarkdown.tsx
+++ src/MendixMarkdown.tsx
@@ -8,7 +8,6 @@ import { MendixMarkdownContainerProps } from "../typings/MendixMarkdownProps";
 import "./ui/MendixMarkdown.scss";
 
 export function MendixMarkdown(props: MendixMarkdownContainerProps): ReactElement {
-    //@ts-ignore
     const { textAttribute, mdeOptions, mdeSpellChecker, mdeHideIcons, mdeToolbar, domRef } = props;
 
     const markdownOptions = useMemo(() => {
@@ -24,17 +23,24 @@ export function MendixMarkdown(props: MendixMarkdownContainerProps): ReactElemen
         (markdownOptions as any)[option.key] = option.value;
     });
 
+    //Create CodeMirror reference so we can use the CodeMirror object later
     const [codemirrorInstance, setCodemirrorInstance] = useState<Editor | null>(null);
     const getCmInstanceCallback = useCallback((editor: Editor) => {
         setCodemirrorInstance(editor);
     }, []);
 
+    /* Create event listener listening for events from outside.
+     * Use a JavaScript function with the following content: document.body.dispatchEvent(new CustomEvent('changeMarkdown' + mdref, { detail: content }));
+     * This will create a custom event triggering a change in the Markdown editor.
+     * Use mdref if you have multiple Markdown editors on the page
+     */
     function changeMarkdown(e: CustomEvent) {
         codemirrorInstance?.replaceSelection(e.detail);
     }
+    const eventListenerHook = "changeMarkdown" + domRef?.value;
     useEffect(() => {
-        document.body.addEventListener("changeMarkdown", changeMarkdown);
-        return () => document.body.removeEventListener("changeMarkdown", changeMarkdown);
+        document.body.addEventListener(eventListenerHook, changeMarkdown);
+        return () => document.body.removeEventListener(eventListenerHook, changeMarkdown);
     });
     return (
         <SimpleMdeReact
--- typings/index.d.ts
+++
@@ -1,7 +0,0 @@
-export {};
-
-declare global {
-    interface Window {
-        foobar: SimpleMDEReact; // 👈️ turn off type checking
-    }
-}
